cd56b99d1c9347d1b7860a40f2060d9a706829c5
jfs-coder <jfs-coder@protonmail.com>
2021-01-02 23:33:05 -0500

    Added Player and Enemy classes which inherit from Ship class. Ship can be drawn and moves around using <wads>

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # credit to TechWithTim (Youtuber) for this code tutorial project.
+# video bookmark: 56:33
 
 import pygame
 import os
@@ -15,7 +16,7 @@ RED_SPACE_SHIP    = pygame.image.load(os.path.join("assets", "pixel_ship_red_sma
 GREEN_SPACE_SHIP  = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
 BLUE_SPACE_SHIP   = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
 
-# Player ship
+# Player Ship
 YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
 
 # Lasers
@@ -27,12 +28,57 @@ YELLOW_LASER      = pygame.image.load(os.path.join("assets", "pixel_laser_yellow
 # Background
 BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
 
+class Ship:
+    def __init__(self, x, y, health=100):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.ship_img = None
+        self.laser_img = None
+        self.lasers = []
+        self.cool_down_counter = 0
+
+    def draw(self, window):
+        window.blit(self.ship_img, (self.x, self.y))
+
+    def get_width(self):
+        return self.ship_img.get_width()
+
+    def get_height(self):
+        return self.ship_img.get_height()
+
+class Player(Ship):
+    def __init__(self, x, y, health = 100):
+        super().__init__(x, y, health)
+        self.ship_img   = YELLOW_SPACE_SHIP
+        self.laser_img  = YELLOW_LASER
+        self.mask       = pygame.mask.from_surface(self.ship_img)
+        self.max_health = health
+
+class Enemy(Ship):
+    COLOR_MAP = {
+        "red": (RED_SPACE_SHIP, RED_LASER),
+        "green": (GREEN_SPACE_SHIP, GREEN_LASER),
+        "blue": (BLUE_SPACE_SHIP, BLUE_LASER)
+    }
+
+    def __init__(self, x, y, color, health = 100):
+        super().__init__(x, y, health)
+        self.ship_img, self.laser_img = self.COLOR_MAP[color]
+        self.mask = pygame.mask.from_surface(self.ship_img)
+
+    def move(self, vel):
+        self.y += vel
+
 def main():
     run   = True
     FPS   = 60
     level = 1
     lives = 5
     main_font = pygame.font.SysFont("comicsans", 50)
+    player_vel = 10
+
+    player = Player(300, 650)
 
     clock = pygame.time.Clock()
 
@@ -45,6 +91,8 @@ def main():
         WIN.blit(lives_label, (10, 10))
         WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
 
+        player.draw(WIN)
+
         pygame.display.update()
         
 
@@ -55,6 +103,16 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a] and player.x + player_vel > 0: # left
+            player.x -= player_vel
+        if keys[pygame.K_d] and player.x + player_vel + player.get_width() < WIDTH: # right
+            player.x += player_vel
+        if keys[pygame.K_w] and player.y - player_vel > 0: # up
+            player.y -= player_vel
+        if keys[pygame.K_s] and player.y + player_vel + player.get_height() < HEIGHT: # down
+            player.y += player_vel
 
 main()
 
