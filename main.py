646358b962753885ddcb6cd5705add0a94072d51
jfs-coder <jfs-coder@protonmail.com>
2021-01-03 22:14:29 -0500

    setup a YOU LOST screen for when out of lives

--- main.py
+++ main.py
@@ -76,16 +76,20 @@ def main():
     level = 0
     lives = 5
     main_font = pygame.font.SysFont("comicsans", 50)
+    lost_font = pygame.font.SysFont("comicsans", 60)
 
     enemies = []
     wave_length = 5
-    enemy_vel = 3
+    enemy_vel = 20
 
     player_vel = 10
 
     player = Player(300, 650)
 
     clock = pygame.time.Clock()
+
+    lost = False
+    lost_count = 0
 
     def redraw_window():
         WIN.blit(BG, (0,0))
@@ -100,11 +104,26 @@ def main():
             enemy.draw(WIN)
 
         player.draw(WIN)
+
+        if lost:
+            lost_label = lost_font.render("You Lost !!!", 1, (255,255,255))
+            WIN.blit(lost_label, (WIDTH/2 - lost_label.get_width()/2, 350))
         
         pygame.display.update()
         
     while run:
         clock.tick(FPS)
+        redraw_window()
+
+        if lives <= 0 or player.health <= 0:
+            lost = True
+            lost_count += 1
+
+        if lost:
+            if lost_count > FPS * 3:
+                run = False
+            else: 
+                continue
         
         if len(enemies) == 0:
             level += 1
@@ -127,10 +146,11 @@ def main():
         if keys[pygame.K_s] and player.y + player_vel + player.get_height() < HEIGHT: # down
             player.y += player_vel
 
-        for enemy in enemies:
+        for enemy in enemies[:]:
             enemy.move(enemy_vel)
-
-        redraw_window()
-
+            if enemy.y + enemy.get_height() > HEIGHT:
+                lives -= 1
+                enemies.remove(enemy)
+        
 main()
 
